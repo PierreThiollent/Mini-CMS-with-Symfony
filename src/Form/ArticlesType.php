e1dcf2e0f57cc9050d2c0b88e78360245e8e2018
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-11 15:21:41 +0100

    feat(): Create form

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Unique;
+
+class ArticlesType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => ['autofocus' => true],
+                'label' => 'Titre',
+                'constraints' => [
+                    new Unique(['message' => 'Ce titre existe déja, veuillez en choisir un autre.']),
+                ],
+            ])
+            ->add('slug', TextType::class, [
+                'label' => 'Slug',
+                'help' => 'Identifiant utilisé pour l\'url de l\'article',
+                'constraints' => [
+                    new Unique(['message' => 'Ce slug existe déja, veuillez en choisir un autre.']),
+                ],
+            ])
+            ->add('content', TextareaType::class, [
+                'attr' => ['rows' => 20],
+                'label' => 'Contenu',
+                'constraints' => [
+                    new NotBlank(['message' => 'Le contenu ne doit pas être vide']),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer'
+            ]);;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
