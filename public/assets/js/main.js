ae90afd523d6c27f0a9eef6d7ca04fea0325ecb7
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-13 13:31:09 +0100

    feat(): Create script for the search

---
+++ public/assets/js/main.js
@@ -0,0 +1,13 @@
+const searchInput = document.getElementById('search');
+const articles = document.querySelectorAll('article');
+
+searchInput.addEventListener('input', event => {
+    articles.forEach(name => {
+        console.log(name);
+        if (name.innerHTML.toLowerCase().includes(event.target.value.toLowerCase())) {
+            name.style.display = 'block';
+        } else {
+            name.style.display = 'none';
+        }
+    });
+});
