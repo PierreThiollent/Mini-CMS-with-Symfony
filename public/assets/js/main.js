67f68bae7168cc215be264f16cce6602c8ee876d
Pierre Thiollent <pierre.thiollent76@gmail.com>
2020-02-13 14:03:35 +0100

    fix(): Remove console.log

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,7 +3,6 @@ const articles = document.querySelectorAll('article');
 
 searchInput.addEventListener('input', event => {
     articles.forEach(name => {
-        console.log(name);
         if (name.innerHTML.toLowerCase().includes(event.target.value.toLowerCase())) {
             name.style.display = 'block';
         } else {
